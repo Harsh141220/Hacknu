6d0610ded8f378976c303f4ffb4f615bcb1d2d5b
Somanshu <somanshS14@gmail.com>
2020-06-03 01:06:30 +0530

    Prototype working fine

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,5 @@
-import 'package:delhimetro/server_connect.dart';
-import 'package:delhimetro/stations.dart';
+import 'package:delhimetro/HomePage.dart';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-import 'slots.dart';
-import 'home.dart';
-import './providers/content.dart';
 
 void main() {
   runApp(MyApp());
@@ -13,26 +8,13 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MultiProvider(
-      providers: [
-        ChangeNotifierProvider(
-          builder: (_) => Content(),
-        ),
-      ],
-      child: MaterialApp(
+    return  MaterialApp(
         theme: ThemeData(
           primarySwatch: Colors.blue,
           accentColor: Colors.black87,
           visualDensity: VisualDensity.adaptivePlatformDensity,
         ),
-        home: Home(),
-        routes: {
-          Slots.routeName: (ctx) => Slots(),
-          Stations.routeName: (ctx) => Stations(),
-          Home.routename: (ctx) => Home(),
-          MyHomePage.routeName: (ctx) => MyHomePage(),
-        },
-      ),
+        home: HomePage(),
     );
   }
 }
